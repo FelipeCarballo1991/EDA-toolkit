9c04a8d7d89e313cc13ba17e89325e5900d1869e
FelipeCarballo1991 <felipecarballo1991@gmail.com>
2025-12-12 15:21:46 -0300

    Add to export ExcelReader

--- pandas_toolkit/io/__init__.py
+++ pandas_toolkit/io/__init__.py
@@ -1,12 +1,12 @@
 from .csv_reader import CSVReader
-# from .excel_reader import ExcelReader
+from .excel_reader import ExcelReader
 # from .json_reader import JSONReader
 # from .file_factory import FileFactory
 from .interfaces import FileReader
 
 __all__ = [
     "CSVReader",
-    # "ExcelReader",
+    "ExcelReader",
     # "JSONReader",
     # "FileFactory",
     "FileReader"
